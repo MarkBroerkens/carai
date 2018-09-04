93bebaa788b7bce33949475f170cf6ddc3d103f0
Mark Broerkens <broerkens gmai.com>
2018-09-04 04:33:00 -0700

    added marks nvidia net

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -80,11 +80,16 @@ class KerasLinear(KerasPilot):
     def __init__(self, model=None, num_outputs=None, *args, **kwargs):
         super(KerasLinear, self).__init__(*args, **kwargs)
         if model:
+            print("Linear NN with given model")
             self.model = model
-        elif num_outputs is not None:
-            self.model = default_n_linear(num_outputs)
+        #elif num_outputs is not None:
+         #   print("Linear NN with n linear")
+         #   self.model = default_n_linear(num_outputs)
         else:
-            self.model = default_linear()
+            print("Linear NN with linear")
+            #self.model = default_linear()
+            #self.model = marks_linear()
+            self.model = marks_nvidia_linear()
 
     def run(self, img_arr):
         img_arr = img_arr.reshape((1,) + img_arr.shape)
@@ -99,13 +104,16 @@ def default_categorical():
     img_in = Input(shape=(120, 160, 3),
                    name='img_in')  # First layer, input layer, Shape comes from camera.py resolution, RGB
     x = img_in
+    x = Cropping2D(cropping=((40, 0), (0, 0)))(x)  # trim 80 pixels off top
     x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu')(
         x)  # 24 features, 5 pixel x 5 pixel kernel (convolution, feauture) window, 2wx2h stride, relu activation
     x = Convolution2D(32, (5, 5), strides=(2, 2), activation='relu')(
         x)  # 32 features, 5px5p kernel window, 2wx2h stride, relu activatiion
-    x = Convolution2D(64, (5, 5), strides=(2, 2), activation='relu')(
+    x = Dropout(.1)(x)  # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
+    x = Convolution2D(64, (5, 5), strides=(1, 1), activation='relu')(
         x)  # 64 features, 5px5p kernal window, 2wx2h stride, relu
-    x = Convolution2D(64, (3, 3), strides=(2, 2), activation='relu')(
+    x = Dropout(.1)(x)  # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
+    x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu')(
         x)  # 64 features, 3px3p kernal window, 2wx2h stride, relu
     x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu')(
         x)  # 64 features, 3px3p kernal window, 1wx1h stride, relu
@@ -114,9 +122,9 @@ def default_categorical():
 
     x = Flatten(name='flattened')(x)  # Flatten to 1D (Fully connected)
     x = Dense(100, activation='relu')(x)  # Classify the data into 100 features, make all negatives 0
-    x = Dropout(.1)(x)  # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
+    x = Dropout(.3)(x)  # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
     x = Dense(50, activation='relu')(x)  # Classify the data into 50 features, make all negatives 0
-    x = Dropout(.1)(x)  # Randomly drop out 10% of the neurons (Prevent overfitting)
+    x = Dropout(.3)(x)  # Randomly drop out 10% of the neurons (Prevent overfitting)
     # categorical output of the angle
     angle_out = Dense(15, activation='softmax', name='angle_out')(
         x)  # Connect every input with every output and output 15 hidden units. Use Softmax to give percentage. 15 categories and find best one based off percentage 0.0-1.0
@@ -188,10 +196,11 @@ def default_catlin():
 
     return model
 
-
-def default_linear():
+def marks_linear():
     img_in = Input(shape=(120, 160, 3), name='img_in')
     x = img_in
+    x = Cropping2D(cropping=((42, 0), (0, 0)))(x)  # trim 40 pixels off top
+    x = Lambda(lambda x: x / 127.5 - 1.)(x)  # normalize and re-center
     x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu')(x)
     x = Convolution2D(32, (5, 5), strides=(2, 2), activation='relu')(x)
     x = Convolution2D(64, (5, 5), strides=(2, 2), activation='relu')(x)
@@ -215,6 +224,79 @@ def default_linear():
                   loss={'angle_out': 'mean_squared_error',
                         'throttle_out': 'mean_squared_error'},
                   loss_weights={'angle_out': 0.5, 'throttle_out': .5})
+    print("Created default_linear model")
+
+    return model
+
+
+def marks_nvidia_linear():
+    '''
+    conv net is inspired by the NVIDIA network described in
+    http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+    I additionally use cropping and dopouts in the fully connecged layers.
+    '''
+    DROPOUT = 0.2
+    img_in = Input(shape=(120, 160, 3), name='img_in')
+    x = img_in
+    x = Cropping2D(cropping=((42, 0), (0, 0)))(x)  # trim 40 pixels off top
+    x = Lambda(lambda x: x / 127.5 - 1.)(x)  # normalize and re-center
+    x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(36, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(48, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(64, (3, 3), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(64, (3, 3), activation='relu')(x)
+
+    x = Flatten(name='flattened')(x)
+    x = Dense(100, activation='linear')(x)
+    x = Dropout(DROPOUT)(x)
+    x = Dense(50, activation='linear')(x)
+    x = Dropout(DROPOUT)(x)
+    x = Dense(10, activation='linear')(x)
+    x = Dropout(DROPOUT)(x)
+    # categorical output of the angle
+    angle_out = Dense(1, activation='linear', name='angle_out')(x)
+
+    # continous output of throttle
+    throttle_out = Dense(1, activation='linear', name='throttle_out')(x)
+
+    model = Model(inputs=[img_in], outputs=[angle_out, throttle_out])
+
+    model.compile(optimizer='adam',
+                  loss={'angle_out': 'mean_squared_error',
+                        'throttle_out': 'mean_squared_error'},
+                  loss_weights={'angle_out': 0.9, 'throttle_out': .1})
+    print("Created marks nvidia model")
+
+    return model
+
+def default_linear():
+    img_in = Input(shape=(120, 160, 3), name='img_in')
+    x = img_in
+    #x = Cropping2D(cropping=((40, 0), (0, 0)))(x)  # trim 52 pixels off top
+    x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(32, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(64, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(64, (3, 3), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu')(x)
+
+    x = Flatten(name='flattened')(x)
+    x = Dense(100, activation='linear')(x)
+    x = Dropout(.1)(x)
+    x = Dense(50, activation='linear')(x)
+    x = Dropout(.1)(x)
+    # categorical output of the angle
+    angle_out = Dense(1, activation='linear', name='angle_out')(x)
+
+    # continous output of throttle
+    throttle_out = Dense(1, activation='linear', name='throttle_out')(x)
+
+    model = Model(inputs=[img_in], outputs=[angle_out, throttle_out])
+
+    model.compile(optimizer='adam',
+                  loss={'angle_out': 'mean_squared_error',
+                        'throttle_out': 'mean_squared_error'},
+                  loss_weights={'angle_out': 0.5, 'throttle_out': .5})
+    print("Created default_linear model")
 
     return model
 
